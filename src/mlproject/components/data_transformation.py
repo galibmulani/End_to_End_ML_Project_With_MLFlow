fc4f0618d3f68fe94c61d51644b8b92559258449
galibmulani <mulanigalib1@gmail.com>
2024-07-09 21:52:40 +0530

    why not saving

--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -16,7 +16,7 @@ class DataTransformation:
         train,test = train_test_split(data)
 
         train.to_csv(os.path.join(self.config.root_dir,"train.csv"),index=False)
-        test.to_csv(os.path.join(self.config.root_dir,"test"),index=False)
+        test.to_csv(os.path.join(self.config.root_dir,"test.csv"),index=False)
 
         logger.info("splited data into train and test sets")
         logger.info(train.shape)
