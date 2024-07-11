de3eff3d102fdca8cb7defb02a509b0487084b5f
galibmulani <mulanigalib1@gmail.com>
2024-07-11 11:24:18 +0530

    model evaluation done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.mlproject.pipeline.stage_04_model_trainer import ModelTrainingPipeline
-
+from src.mlproject.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 STAGE_NAME = "Data Ingestion stage"
 
 try:
@@ -47,3 +47,13 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Model Evaluation stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion=ModelEvaluationPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
